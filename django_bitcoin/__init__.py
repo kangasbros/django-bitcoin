f81e6ea2410a209a5d598a5d384e51fc447d9668
Leo Honkanen <leo.o.honkanen@gmail.com>
2011-08-28 12:56:23 +0300

    update __init__.py

--- django_bitcoin/__init__.py
+++ django_bitcoin/__init__.py
@@ -1,4 +1,4 @@
-from django_bitcoin.models import BitcoinPayment, getNewBitcoinPayment
+from django_bitcoin.models import Payment, getNewBitcoinPayment
 from django_bitcoin.models import BitcoinWallet
 from django_bitcoin.utils import generateuniquehash, int2base64, base642int, bitcoinprice
 
