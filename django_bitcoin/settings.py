bd527a07f3c6beda84793d066172536db150ed7d
Wladston Viana Ferreira Filho <wladston@wladston.net>
2012-06-02 14:23:54 -0300

    Tranfering unconfirmed amounts between wallets.
    Added support for transfering unconfirmed values between wallets, while
    keeping the confirmation status of transferred amounts.
    
    Uses caching to maximize performace.

--- django_bitcoin/settings.py
+++ django_bitcoin/settings.py
@@ -1,55 +1,59 @@
 from django.conf import settings
 
 MAIN_ACCOUNT = getattr(
-    settings, 
-    "BITCOIND_MAIN_ACCOUNT", 
+    settings,
+    "BITCOIND_MAIN_ACCOUNT",
     "somerandomstring14aqqwd")
 BITCOIND_CONNECTION_STRING = getattr(
-    settings, 
-    "BITCOIND_CONNECTION_STRING", 
+    settings,
+    "BITCOIND_CONNECTION_STRING",
     "")
 BITCOIN_PAYMENT_BUFFER_SIZE = getattr(
-    settings, 
+    settings,
     "BITCOIN_PAYMENT_BUFFER_SIZE",
     5)
 BITCOIN_ADDRESS_BUFFER_SIZE = getattr(
-    settings, 
-    "BITCOIN_ADDRESS_BUFFER_SIZE", 
+    settings,
+    "BITCOIN_ADDRESS_BUFFER_SIZE",
     5)
 PAYMENT_VALID_HOURS = getattr(
-    settings, 
-    "BITCOIND_PAYMENT_VALID_HOURS", 
+    settings,
+    "BITCOIND_PAYMENT_VALID_HOURS",
     128)
 REUSE_ADDRESSES = getattr(
-    settings, 
-    "BITCOIND_REUSE_ADDRESSES", 
+    settings,
+    "BITCOIND_REUSE_ADDRESSES",
     True)
 ESCROW_PAYMENT_TIME_HOURS = getattr(
-    settings, 
-    "BITCOIND_ESCROW_PAYMENT_TIME_HOURS", 
+    settings,
+    "BITCOIND_ESCROW_PAYMENT_TIME_HOURS",
     4)
 ESCROW_RELEASE_TIME_DAYS = getattr(
-    settings, 
-    "BITCOIND_ESCROW_RELEASE_TIME_DAYS", 
+    settings,
+    "BITCOIND_ESCROW_RELEASE_TIME_DAYS",
     14)
 BITCOIN_MINIMUM_CONFIRMATIONS = getattr(
-    settings, 
-    "BITCOIN_MINIMUM_CONFIRMATIONS", 
+    settings,
+    "BITCOIN_MINIMUM_CONFIRMATIONS",
     3)
 BITCOIN_TRANSACTION_CACHING = getattr(
-    settings, 
-    "BITCOIN_TRANSACTION_CACHING", 
+    settings,
+    "BITCOIN_TRANSACTION_CACHING",
     False)
 BITCOIN_TRANSACTION_SIGNALING = getattr(
-    settings, 
-    "BITCOIN_TRANSACTION_SIGNALING", 
+    settings,
+    "BITCOIN_TRANSACTION_SIGNALING",
     False)
-
 BITCOIN_CURRENCIES = getattr(
-    settings, 
+    settings,
     "BITCOIN_CURRENCIES",
     [
         "django_bitcoin.currency.BTCCurrency",
         "django_bitcoin.currency.EURCurrency",
         "django_bitcoin.currency.USDCurrency"
         ])
+# Allow transfer of unconfirmed ammounts between wallets
+BITCOIN_UNCONFIRMED_TRANSFERS = getattr(
+    settings,
+    "BITCOIN_UNCONFIRMED_TRANSFERS",
+    False)
