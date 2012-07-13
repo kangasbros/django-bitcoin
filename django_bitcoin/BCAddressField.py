e5f20444aa3ee8160ded5c25abc4dbbe6567a0b0
Jeremias Kangas <jeremias.kangas@gmail.com>
2012-07-13 12:08:07 +0200

    b36 encode/decode added

--- django_bitcoin/BCAddressField.py
+++ django_bitcoin/BCAddressField.py
@@ -91,6 +91,28 @@ def b58decode(v, length):
     return result
 
 
+def b36encode(number, alphabet='0123456789abcdefghijklmnopqrstuvwxyz'):
+    """Converts an integer to a base36 string."""
+    if not isinstance(number, (int, long)):
+        raise TypeError('number must be an integer')
+
+    base36 = '' if number != 0 else '0'
+    sign = ''
+    if number < 0:
+        sign = '-'
+        number = -number
+
+    while number != 0:
+        number, i = divmod(number, len(alphabet))
+        base36 = alphabet[i] + base36
+
+    return sign + base36
+
+
+def b36decode(number):
+    return int(number, 36)
+
+
 def get_bcaddress_version(strAddress):
     """ Returns None if strAddress is invalid.    Otherwise returns integer version of address. """
     addr = b58decode(strAddress, 25)
